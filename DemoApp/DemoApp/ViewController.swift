0206b83a8f1ac43ab4b1561a41bba37d7a8d60a4
vidyalay <developer1.sapphire@gmail.com>
2021-03-04 18:23:03 +0530

    sam

--- DemoApp/DemoApp/ViewController.swift
+++ DemoApp/DemoApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Test")
+        print("sam test")
         // Do any additional setup after loading the view.
     }
 
