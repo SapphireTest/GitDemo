be226303c4a14df58b5c7b33097b6e3f0ec20c29
Anisha <developer1.sapphire@gmail.com>
2021-03-04 18:53:03 +0530

    print

--- DemoApp/DemoApp/ViewController.swift
+++ DemoApp/DemoApp/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Test")
         print("sam testing")
+        print("Test")
+        print("sam testing")
         // Do any additional setup after loading the view.
     }
 
